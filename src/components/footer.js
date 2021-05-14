8a39312988c90b9bd01766c8f9fac7a3cd24ddba
Rob Uridge <rob@uridge.com>
2021-05-14 16:14:57 +0800

    feat(Layout): initial layout structure

---
+++ src/components/footer.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+
+function Footer(props) {
+  return <footer>Footer Copyright info.</footer>;
+}
+
+Footer.propTypes = {};
+
+export default Footer;
---
+++ src/components/main-nav-bar.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+
+function MainNavBar(props) {
+  return <nav>Navigation: Home – Projects – Snippets</nav>;
+}
+
+MainNavBar.propTypes = {};
+
+export default MainNavBar;
