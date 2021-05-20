b21bd7c27632bc9f96c911810587af28b6f04f23
Rob Uridge <rob@uridge.com>
2021-05-20 23:09:22 +0800

    feat: 🎸 add 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,54 +1,37 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import * as React from "react";
+import { Link } from "gatsby";
+import Seo from "../components/seo";
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-// markup
-const NotFoundPage = () => {
+function NotFoundPage() {
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
+    <>
+      <Seo title="404: Page not found" />
+      <div className="flex flex-col min-h-screen">
+        <div class="flex-1 w-full flex flex-col justify-center max-w-screen-md px-6 mx-auto">
+          <h1 className="py-10 text-6xl font-bold lg:text-8xl">
+            Page not found
+          </h1>
+          <p className="text-3xl lg:text-4xl">
+            Sorry{" "}
+            <span role="img" aria-label="Pensive emoji">
+              😔
+            </span>{" "}
+            I couldn’t find what you were looking for.
+          </p>
+        </div>
+        <div
+          class="max-w-screen-md px-6 mx-auto w-full pt-10"
+          style={{ "padding-bottom": "20vh" }}
+        >
+          <p className="text-3xl lg:text-4xl">
+            <Link to="/" className="text-blue-700 hover:underline">
+              Go to the homepage
+            </Link>
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default NotFoundPage
+export default NotFoundPage;
